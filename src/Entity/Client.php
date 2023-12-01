eae18903e2036da14457ac64043b87bdda1a96c7
laurasullyalexandrine <launyie@gmail.com>
2023-12-01 14:36:08 +0100

    SymfonyInsight analyse fix

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -118,7 +118,6 @@ class Client implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
     }
 
     public function getName(): ?string
